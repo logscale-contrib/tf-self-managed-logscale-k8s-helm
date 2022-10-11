9067c77c4589caca9f1c072a079c2f7cd91e3328
Ryan Faircloth <ryan@dss-i.com>
2022-10-11 16:47:13 -0400

    feat: external provider.tf

--- variables.tf
+++ variables.tf
@@ -2,22 +2,6 @@ variable "uniqueName" {
   type        = string
   description = "(optional) describe your variable"
 }
-variable "eks_cluster_id" {
-  type        = string
-  description = "(optional) describe your variable"
-}
-variable "eks_endpoint" {
-  type        = string
-  description = "(optional) describe your variable"
-}
-variable "eks_cluster_certificate_authority_data" {
-  type        = string
-  description = "(optional) describe your variable"
-}
-variable "eks_oidc_provider_arn" {
-  type        = string
-  description = "(optional) describe your variable"
-}
 
 variable "repository" {
   type        = string
