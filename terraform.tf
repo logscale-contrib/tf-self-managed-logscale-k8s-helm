09265abe3a1203c1e5e17914b010f4e830b7def6
Ryan Faircloth <ryan@dss-i.com>
2023-05-14 09:39:17 -0400

    fix: remove unused providers

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,9 @@
 terraform {
   required_version = ">= 1.1.0"
   required_providers {
-    aws = {
-      version = ">= 2.28.1"
-    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14"
-    }
-    kubernetes = {
-      version = "~> 2.11"
     }
 
   }
