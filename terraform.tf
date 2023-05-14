417d8514292df98d099282b200348d36c6912468
Ryan Faircloth <ryan@dss-i.com>
2023-05-14 09:59:52 -0400

    fix: provider

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,9 @@
 terraform {
   required_version = ">= 1.1.0"
   required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.20.0"
     }
-
   }
 }
